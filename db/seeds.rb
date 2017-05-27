7c19d1569ffe376b83fdf0fac1309e7951354478
Long Nguyen <long.polyglot@gmail.com>
2017-05-27 21:39:10 +0700

    Add sample models and data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+1.upto(3).each do
+  Company.create!(
+    name: Faker::Company.name,
+    description: Faker::Company.catch_phrase
+  )
+end
+
+1.upto(20).each do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    company_id: [1,2,3].sample
+  )
+end
